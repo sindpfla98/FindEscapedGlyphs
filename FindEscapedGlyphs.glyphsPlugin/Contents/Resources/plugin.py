4be5664f4f661c0b776ace1b3a8a2c4b102db504
sindpfla98 <43367995+sindpfla98@users.noreply.github.com>
2023-11-29 14:18:10 +0900

    first commit

---
+++ FindEscapedGlyphs.glyphsPlugin/Contents/Resources/plugin.py
@@ -0,0 +1,104 @@
+# encoding: utf-8
+
+###########################################################################################################
+#
+#
+#	General Plugin
+#
+#	Read the docs:
+#	https://github.com/schriftgestalt/GlyphsSDK/tree/master/Python%20Templates/General%20Plugin
+#
+#
+###########################################################################################################
+
+from __future__ import division, print_function, unicode_literals
+import objc
+from GlyphsApp import *
+from GlyphsApp.plugins import *
+import os
+
+class FindEscapedGlyphs(GeneralPlugin):
+	window = objc.IBOutlet()
+	MinX_tf = objc.IBOutlet()
+	MinY_tf = objc.IBOutlet()
+	MaxX_tf = objc.IBOutlet()
+	MaxY_tf = objc.IBOutlet()
+	result_tf = objc.IBOutlet()
+
+	selectedGlyphs_ckb = objc.IBOutlet()
+
+	@objc.python_method
+	def settings(self):
+
+		self.name = Glyphs.localize({
+			'en': 'Find Escaped Glyphs',
+			'ko': '벗어난 글립 찾기',
+			})
+		self.loadNib('IBdialog', __file__)
+		return
+
+	@objc.python_method
+	def start(self):
+		try:
+			try:
+				newMenuItem = NSMenuItem(self.name, self.showWindow_)
+				Glyphs.menu[GLYPH_MENU].append(newMenuItem)
+			except:
+				mainMenu = Glyphs.mainMenu()
+				s = objc.selector(self.showWindow_, signature='v@:@')
+				newMenuItem = NSMenuItem.alloc().initWithTitle_action_(self.name, s)
+				newMenuItem.setTarget_(self)
+				mainMenu.itemWithTag_(3).submenu().addItem_(newMenuItem)
+		except:
+			pass
+
+	@objc.python_method
+	def showWindow_(self, sender):
+		self.window.makeKeyAndOrderFront_(self)
+
+	@objc.python_method
+	def __file__(self):
+		"""Please leave this method unchanged"""
+		return __file__
+
+	@objc.IBAction
+	def setApplyButtonClicked_(self, sender):
+		MinX = int(self.MinX_tf.stringValue())
+		MinY = int(self.MinY_tf.stringValue())
+		MaxX = int(self.MaxX_tf.stringValue())
+		MaxY = int(self.MaxY_tf.stringValue())
+		result = 'Glyph Name'
+		selected = self.selectedGlyphs_ckb.state()
+
+		if selected:
+			for layer in Glyphs.font.selectedLayers:
+				result = checkLayer(self, layer, MinX, MinY, MaxX, MaxY, result)
+
+		else:
+			for glyph in Glyphs.font.glyphs:
+				for layer in glyph.layers:
+					if layer.master == Glyphs.font.selectedFontMaster:
+						result = checkLayer(self, layer, MinX, MinY, MaxX, MaxY, result)
+		
+		result += ('\n' + 'Done')
+		self.result_tf.setString_(result)
+				
+
+	@objc.IBAction
+	def setCancleButtonClicked_(self, sender):
+		self.window.close()
+
+def checkLayer(self, layer, MinX, MinY, MaxX, MaxY, result):
+	print_result = False
+	if layer.bounds.origin.x < MinX:
+		print_result = True
+	if layer.bounds.origin.y < MinY:
+		print_result = True
+	if layer.bounds.origin.x + layer.bounds.size.width > MaxX:
+		print_result = True
+	if layer.bounds.origin.y + layer.bounds.size.height > MaxY:
+		print_result = True
+	if print_result:
+		result += ('\n' + layer.parent.name)
+		
+	return result
